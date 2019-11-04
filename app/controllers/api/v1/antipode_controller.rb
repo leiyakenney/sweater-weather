24e029529f420f5a5637a5bbe15b333e2d24b1db
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 10:45:25 -0700

    Remove pry

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -4,8 +4,7 @@ class Api::V1::AntipodeController < ApplicationController
     coordinates = LocationFacade.new(params[:location]).coordinates
     antipode_coordinates = AntipodeFacade.new(coordinates).antipode_coordinates
     weather = AntipodeForecastFacade.new(antipode_coordinates).create_forecast
-    binding.pry
-    forecast = Forecast.new(weather)
-    render json: ForecastSerializer.new(forecast)
+    forecast = Antipode.new(weather)
+    render json: AntipodeSerializer.new(forecast)
   end
 end
