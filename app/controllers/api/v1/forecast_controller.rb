59dd26d0c13f823fac7d949eb34d03c4132961a2
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-03 17:54:34 -0700

    Creates forecast#show method

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
 
-  def index
-  end 
+  def show
+    coordinates = LocationFacade.new(params[:location]).coordinates
+    forecast = ForecastFacade.new(coordinates).forecast
+    render json: ForecastSerializer.new(forecast)
+  end
 end
