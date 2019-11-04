6565da5506e1421be54c35a5700fd3b509f89a70
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-03 21:40:57 -0700

    Updates controller to use PORO

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::ForecastController < ApplicationController
 
   def show
     coordinates = LocationFacade.new(params[:location]).coordinates
-    forecast = ForecastFacade.new(coordinates).forecast
+    weather = ForecastFacade.new(coordinates).forecast
+    forecast = Forecast.new(weather)
+    binding.pry
     render json: ForecastSerializer.new(forecast)
   end
 end
