d399a04568f7daa6ff944d110609eb5c4ddfbec1
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 09:56:08 -0700

    Add .antipode_coordinates to end of antipode_coordinates method

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::AntipodeController < ApplicationController
 
   def show
     coordinates = LocationFacade.new(params[:location]).coordinates
-    antipode_coordinates = AntipodeFacade.new(coordinates)
+    antipode_coordinates = AntipodeFacade.new(coordinates).antipode_coordinates
+    binding.pry
     weather = ForecastFacade.new(antipode_coordinates).create_forecast
     forecast = Forecast.new(weather)
     render json: ForecastSerializer.new(forecast)
