12df8f1444094a93b2d73f6757a914d7ab031476
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 09:29:12 -0700

    Create antipode controller

---
+++ app/controllers/api/v1/antipode_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::AntipodeController < ApplicationController
+
+  def show
+    coordinates = LocationFacade.new(params[:location]).coordinates
+    antipode_coordinates = AntipodeFacade.new(coordinates)
+    weather = ForecastFacade.new(antipode_coordinates).create_forecast
+    forecast = Forecast.new(weather)
+    render json: ForecastSerializer.new(forecast)
+  end
+end
