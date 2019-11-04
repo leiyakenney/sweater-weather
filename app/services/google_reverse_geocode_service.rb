a555272af683b4c685ed44261c75ca3c3fc83af6
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 11:18:38 -0700

    Create google reverse geocode service

---
+++ app/services/google_reverse_geocode_service.rb
@@ -0,0 +1,26 @@
+class GoogleReverseGeocodeService
+  def initialize(lat, long)
+    @lat = lat
+    @long = long
+  end
+
+  def get_location_data
+    binding.pry
+    response = conn.get("json") do |req|
+      req.params[:latlng] = "#{@lat},#{@long}"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def city_name
+    get_location_data[:results][0][:formatted_address]
+  end
+
+  private
+  def conn
+    Faraday.new(
+      url: 'https://maps.googleapis.com/maps/api/geocode/',
+      params: {'key' => ENV['GOOGLE_API_KEY']}
+    )
+  end
+end
