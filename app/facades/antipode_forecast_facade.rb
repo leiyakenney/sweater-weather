3d4c092f10cf56141b4b3c3061c8379ba3bc4c95
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 10:28:01 -0700

    Make AntipodeForecastFacade

---
+++ app/facades/antipode_forecast_facade.rb
@@ -0,0 +1,17 @@
+class AntipodeForecastFacade
+  attr_reader :id
+
+  def initialize(antipode_coordinates)
+    @id = rand(0..99)
+    @antipode_coords = antipode_coordinates
+  end
+
+  def darksky_service
+    DarkskyAmypodeService.new(@antipode_coords)
+  end
+
+  def create_forecast
+    data = darksky_service.get_forecast_data
+    binding.pry
+  end
+end
