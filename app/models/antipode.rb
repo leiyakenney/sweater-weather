bc336efae2610ad05b8a64170f360dc8538d4638
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 11:54:09 -0700

    Format forecast

--- app/models/antipode.rb
+++ app/models/antipode.rb
@@ -1,18 +1,20 @@
 class Antipode
   attr_reader :id,
               :coordinates,
-              :summary,
-              :current_temperature,
               :search_location,
-              :location_name
+              :location_name,
+              :forecast
 
   def initialize(weather, search_location, antipode_location)
     @id = rand(0..99)
     @coordinates = "#{weather[:latitude]}, #{weather[:longitude]}"
     @currently = weather[:currently]
-    @summary = @currently[:summary]
-    @current_temperature = @currently[:temperature].round
     @search_location = search_location
     @location_name = antipode_location
+    @forecast =
+      {
+        summary: weather[:currently][:summary],
+        current_temperature: weather[:currently][:temperature].round
+      }
   end
 end
