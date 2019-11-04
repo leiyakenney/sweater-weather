2dcb19bc7b72ba06b7d17a96c58d33076f91ae5c
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 09:55:44 -0700

    Update Faraday for Amypode

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -2,22 +2,25 @@ class AmypodeService
   def initialize(coordinates)
     binding.pry
     @lat = coordinates[:lat]
-    @long = coordinates[:long]
+    @long = coordinates[:lng]
   end
 
   def get_antipode_data
     response = conn.get("json") do |req|
       req.params[:lat] = @lat
       req.params[:long] = @long
+      req.headers['api_key'] = ENV['AMYPODE_API_KEY']
     end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
   def conn
-    Faraday.new(
-      url: 'http://amypode.herokuapp.com/api/v1/antipodes',
-      header: {'key' => ENV['AMYPODE_API_KEY']}
-    )
+    Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/antipodes') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:lat] = @lat
+      f.params[:long] = @long
+      f.headers[:api_key] = ENV['AMYPODE_API_KEY']
+    end
   end
 end
