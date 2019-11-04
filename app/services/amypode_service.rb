03610a8c419cc0ea9149196b1e8a982309f5cf97
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 11:02:19 -0700

    Refactor Amypode Service

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -5,15 +5,26 @@ class AmypodeService
   end
 
   def get_antipode_data
+    response = conn.get("/api/v1/antipodes")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def coordinates
+    get_antipode_data[:data][:attributes]
+  end
+
+  def city_name
+    binding.pry
+  end
+
+  private
+  def conn
     conn = Faraday.new(url: "http://amypode.herokuapp.com") do |f|
       f.headers["api_key"] = ENV['AMYPODE_API_KEY']
       f.params["lat"] = @lat
       f.params["long"] = @lng
       f.adapter Faraday.default_adapter
     end
-
-    response = conn.get("/api/v1/antipodes")
-
-    JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
   end
 end
