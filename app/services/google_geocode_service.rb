db95c0bc93540d884b2377bb6b1ffe9baf7d710b
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-03 17:55:21 -0700

    Creates Google Geocode Service

---
+++ app/services/google_geocode_service.rb
@@ -0,0 +1,21 @@
+class GoogleGeocodeService
+  def initialize(location)
+    @city = location.split(",")[0]
+    @state = location.split(",")[1]
+  end
+
+  def get_location_data
+    response = conn.get("json") do |req|
+      req.params[:address] = "#{@city}+#{@state}"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  def conn
+    Faraday.new(
+      url: 'https://maps.googleapis.com/maps/api/geocode/',
+      params: {'key' => ENV['GOOGLE_API_KEY']}
+    )
+  end
+end
