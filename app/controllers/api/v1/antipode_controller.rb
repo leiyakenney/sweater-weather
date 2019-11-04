c57806431d3595e12a39458d83eba0da06ca769e
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-04 11:01:08 -0700

    Add additional params and locations

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,9 +2,12 @@ class Api::V1::AntipodeController < ApplicationController
 
   def show
     coordinates = LocationFacade.new(params[:location]).coordinates
+    search_location = LocationFacade.new(params[:location]).search_city
     antipode_coordinates = AntipodeFacade.new(coordinates).antipode_coordinates
+    antipode_location = AntipodeFacade.new(coordinates).antipode_city
+    binding.pry
     weather = AntipodeForecastFacade.new(antipode_coordinates).create_forecast
-    forecast = Antipode.new(weather)
+    forecast = Antipode.new(weather, search_location, antipode_coordinates)
     render json: AntipodeSerializer.new(forecast)
   end
 end
