ff0e49d664a1288c621bbc6857a1c91186c07b72
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-05 14:17:36 -0700

    Create antipode request test

--- spec/requests/api/v1/antipode_request_spec.rb
+++ spec/requests/api/v1/antipode_request_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-describe "Amypode API" do
-  it "sends a list of antipodes" do
-    create_list(:antipode, 2)
+describe "Antipode Endpoint" do
+  it "shows the search city, antipode city, and the antipode city's weather" do
+    get '/api/v1/antipode?location=hongkong'
 
-    get '/api/v1/antipode'
+    antipode = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-
-    antipode = JSON.parse(response.body)
+    expect(response.content_type).to eq('application/json')
+    expect(antipode.class).to eq(Hash)
+    expect(antipode[:data].count).to eq(3)
+    expect(antipode[:data][:attributes][:search_location]).to eq("Hong Kong")
+    expect(antipode[:data][:attributes][:location_name]).to eq("Jujuy")
+    expect(antipode[:data][:type]).to eq('antipode')
   end
 end
