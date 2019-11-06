8b049507acce13e93b7d84a47e656db75e5c1501
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-06 05:55:55 -0700

    Adds simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,13 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  require 'simplecov'
+
+  SimpleCov.start 'rails' do
+    add_filter '/bin/'
+    add_filter '/db/'
+    add_filter '/spec/'
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
