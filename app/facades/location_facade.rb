4d1489fc3eddbd6d5f3631e34a4847b39a8ddb7d
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-06 07:43:05 -0700

    Delete unused search_city method

--- app/facades/location_facade.rb
+++ app/facades/location_facade.rb
@@ -11,8 +11,4 @@ class LocationFacade
     data = google_service.location_data(@location)
     data[:results][0][:geometry][:location]
   end
-
-  def search_city
-    google_service.city_name
-  end
 end
